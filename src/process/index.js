85efba94ab93f06bc65d0013adb9c87d5c40aa23
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2024-10-07 20:48:04 +0300

    Update index.js

--- src/process/index.js
+++ src/process/index.js
@@ -1,10 +1,7 @@
 const rgb = (r, g, b, msg) => `\x1b[38;2;${r};${g};${b}m${msg}\x1b[0m`;
 const log = (...args) => console.log(`[${rgb(88, 101, 242, 'arRPC')} > ${rgb(237, 66, 69, 'process')}]`, ...args);
 
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-
-const DetectableDB = require('./detectable.json');
+import DetectableDB from "./detectable.json" assert { type: "json" };
 
 import * as Natives from './native/index.js';
 const Native = Natives[process.platform];
