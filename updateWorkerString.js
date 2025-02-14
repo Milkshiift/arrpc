c04a2587ec0ced9cfe357c742d688d50b5f8c1d3
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-02-14 20:53:53 +0200

    Use bundled scanner worker

--- updateWorkerString.js
+++ updateWorkerString.js
@@ -1,6 +1,17 @@
 import {readFile, writeFile} from "node:fs/promises";
 
-let workerCode = await readFile("./src/process/scannerWorker.js", "utf8");
+const bundleResult = await Bun.build({
+    minify: true,
+    sourcemap: "none",
+    format: "esm",
+    target: "node",
+    entrypoints: ["./src/process/scannerWorker.js"],
+    outdir: "build",
+    packages: "bundle",
+});
+if (bundleResult.logs.length) console.log(bundleResult.logs);
+
+let workerCode = await readFile("./build/scannerWorker.js", "utf8");
 workerCode = workerCode.replace(/\\/g, '\\\\');
 workerCode = workerCode.replace(/\$\{/g, '\\${');
 workerCode = workerCode.replace(/`/g, '\\`');
